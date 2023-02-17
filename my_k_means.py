08e71ed1c5b83414b320da079202a2f0a3db66ae
Gregor Karetka <karetka.gregor@gmail.com>
2023-02-17 14:41:06 +0100

    update my K means implementation

--- my_k_means.py
+++ my_k_means.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def eu_dist(X, pts, Ks):
+def eu_dist(x, pts, ks, dim):
     """Returns eu_dist from Ks pts
 
     Args:
@@ -16,12 +16,12 @@ def eu_dist(X, pts, Ks):
         Ks (_type_): number of centroids
 
     Returns:
-        _type_: n x Ks array of dist from centroids
+        _type_: n x ks array of dist from centroids
     """
-    diff = np.repeat(X, Ks, axis=1) - np.reshape(pts, newshape=(1,4), order='F')
-    diff = np.reshape(diff, newshape=(400, 2), order='F')
+    diff = np.repeat(x, ks, axis=1) - np.reshape(pts, newshape=(1, dim*ks), order='F')
+    diff = np.reshape(diff, newshape=(200*ks, 2), order='F')
     norms = np.linalg.norm(diff, axis=1)
-    return np.reshape(norms, newshape=(200, 2), order='F')
+    return np.reshape(norms, newshape=(200, ks), order='F')
 
 
 df = pd.read_csv('./data/data.csv')
@@ -29,15 +29,18 @@ df.drop_duplicates(inplace=True)
 
 # using only Spending_Score and income variable for easy visualisation
 X = df.iloc[:, [2, 3]].values
-K = 2
+K = 7
+dim = 2
 
-start_points = [X[15], X[16]]
+np.random.seed(seed=42)
+start_points = X[np.random.randint(0, X.shape[0], size=(K,1))]
 cur_clusters = []
 prev_clusters = []
 
-n = 100
+N = 100
+n = N
 while n > 0:
-    eucl_dist = eu_dist(X, start_points, K)
+    eucl_dist = eu_dist(X, start_points, K, dim)
     closest_cluster_map = np.argmin(eucl_dist, axis=1)
 
     # for every cluster
@@ -46,11 +49,11 @@ while n > 0:
     start_points = []
     for i in range(K):
         cur_clusters.append(np.where(closest_cluster_map == i))
-        cur_clust_points = eucl_dist[cur_clusters[i]]
+        cur_clust_points = X[cur_clusters[i]]
         cur_clust_centroids = np.mean(cur_clust_points.T, axis=1)
         start_points.append(cur_clust_centroids)
 
-    print(f"Iter: {n}: {start_points}")
+    #print(f"Iter: {N-n}: {start_points}")
     n -= 1
 
 result = None
@@ -73,13 +76,12 @@ resulting_data = np.c_[X[:], sorted_result[:]]
 # display
 MARKER_SIZE = 100
 res_df = pd.DataFrame(data=resulting_data, columns=['c0', 'c1', 'c2', 'c3'])
-print(res_df.head())
+
+res_df_centroids = pd.DataFrame(data=start_points, columns=['x', 'y'])
 
 plt.figure(figsize=(15, 7))
-sns.scatterplot(data=res_df[res_df['c3'] == 1], x='c0', y='c1',
-                color = 'red', label = 'Cluster 1',s=MARKER_SIZE)
-sns.scatterplot(data=res_df[res_df['c3'] == 0], x='c0', y='c1',
-                color = 'blue', label = 'Cluster 2',s=MARKER_SIZE)
+sns.scatterplot(data=res_df, x='c0', y='c1', hue='c3', palette=sns.color_palette(), s=MARKER_SIZE)
+sns.scatterplot(data=res_df_centroids, x='x', y='y', color='red', s=5*MARKER_SIZE)
 
 plt.grid(False)
 plt.title('Clusters of customers')
